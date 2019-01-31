be618d8ff82a5ebcd12d9b0041b80bd3dff705a9
Peter Enescu <peter.enescu@gmail.com>
2019-01-31 08:34:08 -0800

    Completed Lab 4

---
+++ routes/project.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET home page.
+ */
+
+exports.viewProject = function(req, res) { 
+  var name = req.params.name; 
+  console.log("The project name is: " + name);
+  res.render('project', {
+    'projectName': name
+  });
+};
